63a1fa9aed571410f4aaf6d5e17ebfb3fd10d6ef
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-04-23 21:07:11 +0200

    add tomma  Dtos-klasser, Service-klasser och dess Imp-klasser

--- src/main/java/com/example/pensionat/Pensionat/Dtos/RumDto.java
+++ src/main/java/com/example/pensionat/Pensionat/Dtos/RumDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//Har skapar Dto-klassar, vi kan ändra innehåller och lägga
+// till fler droklasser om det behövs utifrån vårt behov
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
