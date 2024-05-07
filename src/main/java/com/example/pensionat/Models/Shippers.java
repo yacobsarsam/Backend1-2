cefddbe1b4e1b8b7ff222db4f2a3b75ef90de8ac
Eric Brolin <eric.brolin@yh.nackademin.se>
2024-05-07 09:01:44 +0200

    added model Shippers

---
+++ src/main/java/com/example/pensionat/Models/Shippers.java
@@ -0,0 +1,33 @@
+package com.example.pensionat.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Shippers {
+
+    @Id
+    @GeneratedValue
+    protected long id;
+    @Column(nullable = false)
+    protected String companyName;
+    @Column(nullable = false)
+    protected String phone;
+
+    public Shippers(String companyName, String phone) {
+        this.companyName = companyName;
+        this.phone = phone;
+    }
+
+}
+
