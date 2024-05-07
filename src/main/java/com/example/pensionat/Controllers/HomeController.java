d7157a2f216cf29b5c321604aaca9e476ea2a49e
Joakim Hansen <jocke.hansen@outlook.com>
2024-05-07 18:29:55 +0200

    Börjat byggandet av thymeleaf tabellen

--- src/main/java/com/example/pensionat/Controllers/HomeController.java
+++ src/main/java/com/example/pensionat/Controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("1")
+    public String test(){
+        return "visaAvtalsKunder";
+    }
 }
