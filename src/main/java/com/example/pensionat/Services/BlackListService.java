59e8ac4266fe866c7d81674821b75392413c903b
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-05-09 15:10:47 +0200

    add new lackListPerson to blacklist med auto genererad ID

---
+++ src/main/java/com/example/pensionat/Services/BlackListService.java
@@ -0,0 +1,19 @@
+package com.example.pensionat.Services;
+
+import com.example.pensionat.Models.BlackListPerson;
+import org.springframework.ui.Model;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BlackListService {
+     public List<BlackListPerson> getAllBLKunder() throws IOException;
+
+     boolean isCustomerFieldsFilledAndCorrect(String name, String email, String group);
+
+     boolean checkIfBLKundExistByEmailUtanAttSkapa(String email) throws IOException;
+
+     void AddBLKundToBlackList(String name, String email, String group);
+
+     String getAllAvailableBLKundInfo(String name, String email, String group, Model model);
+}
