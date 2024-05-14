ca3ae80b6e00cd5ab18830defa4369e5184e86e0
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-05-14 21:56:19 +0200

    Skapat klasser till olika rumevent samt "huvudEvent klass"

---
+++ src/main/java/com/example/pensionat/Models/RoomCleaningFinished.java
@@ -0,0 +1,6 @@
+package com.example.pensionat.Models;
+
+//TODO ändra om koden om det behövs
+public class RoomCleaningFinished extends EventBase {
+    String cleaninigByUser;
+}
---
+++ src/main/java/com/example/pensionat/Models/RoomClosed.java
@@ -0,0 +1,5 @@
+package com.example.pensionat.Models;
+
+//TODO ändra om koden om det behövs
+public class RoomClosed extends EventBase {
+}
