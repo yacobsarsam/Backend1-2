6e145686f895c3808e6e93d23866627ecc37d498
Eric Brolin <eric.brolin@yh.nackademin.se>
2024-05-26 12:28:22 +0200

    now sends a html-file as a mail to the customer which has booked a room

--- src/main/java/com/example/pensionat/Security/Services/Imp/CustomerMailServiceImp.java
+++ src/main/java/com/example/pensionat/Security/Services/Imp/CustomerMailServiceImp.java
@@ -5,14 +5,24 @@ import com.example.pensionat.Security.Services.CustomerMailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerMailServiceImp implements CustomerMailService {
+
     private final JavaMailSender mailSender;
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
 
 
     @Override
@@ -20,7 +30,9 @@ public class CustomerMailServiceImp implements CustomerMailService {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
-        String htmlMsg = "<h3>'temporary HTML mail message'</h3>"; //ändra till html-fil?
+//        String htmlMsg = "<h3>'temporary HTML mail message'</h3>"; //ändra till html-fil?
+
+        String htmlMsg = renderTemplate("htmlConfirmationMailTemplate", b);
 
         helper.setText(htmlMsg, true);
         helper.setTo(b.getKund().getEmail());
@@ -28,4 +40,27 @@ public class CustomerMailServiceImp implements CustomerMailService {
         helper.setFrom("noreply@example.com");
         mailSender.send(mimeMessage);
     }
+
+    public String renderTemplate(String templateName, Bokning b) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("title", "Bokningsbekräftelse");
+        variables.put("name", b.getKund().getNamn());
+        variables.put("email", b.getKund().getEmail());
+        variables.put("phone-number", b.getKund().getTel());
+        variables.put("doubleRoom", b.getRum().isDubbelrum());
+        variables.put("startDate", b.getStartdatum());
+        variables.put("endDate", b.getSlutdatum());
+        variables.put("totalPrice", b.getTotalPrice());
+        variables.put("numOfBeds", b.getNumOfBeds());
+        variables.put("date", new java.util.Date());
+        return renderTemplate(templateName, variables);
+    }
+
+    public String renderTemplate(String templateName, Map<String, Object> variables) {
+        Context context = new Context();
+        context.setVariables(variables);
+        return templateEngine.process(templateName, context);
+    }
 }
+
+
